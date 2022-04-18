57e657e8ac3c382a1262ebf32ebdcc2af8e0ef7a
reinaldodu <reinaldodu@gmail.com>
2022-04-18 18:35:17 -0500

    fix bug

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://reinaldodu.github.io/',
+  base: 'https://reinaldodu.github.io/gh-actions/',
   plugins: [react()]
 })
